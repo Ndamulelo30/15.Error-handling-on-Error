8402f9ea7fdc817960ea423a995b59ccf74d0604
Ndamulelo30 <141257791+Ndamulelo30@users.noreply.github.com>
2024-02-23 01:56:40 +0200

    Add files via upload

---
+++ 13.Error Handling/13.Error Handling try catch/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim userInput As String = TextBox1.Text
+
+        On Error GoTo ErrorHandler
+        Dim number As Integer = Integer.Parse(userInput)
+        MessageBox.Show("You entered: " & number)
+
+        Exit Sub
+
+ErrorHandler:
+        MessageBox.Show("Error: Please enter a valid number.")
+    End Sub
+End Class
